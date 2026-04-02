704df142f68ab3c0558c11b4a1f0c0b4ff42bfe1
hohimwong <114516847+hohimwong@users.noreply.github.com>
2026-04-02 12:43:03 +0100

    Update 2D_JW_alpha2omega_v3.jl

--- 2D_JW_alpha2omega_v3.jl
+++ 2D_JW_alpha2omega_v3.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 # note that the "y" here corresponds to "z" in the actual model, and the code no longer has a switch for source terms !
 # the 2D codes also use n for x and m for y
 
-function u(invar; amp=0.0, phase=0.0, freq = 1.7, l=i, m=m_pts, n=n_pts)       # function taking in ω0 and spits out ω with time/space dependence (3d array)
+function u(invar; amp=0.0, phase=0.0, freq = 1.7, l=i, m=m_pts, n=n_pts)       # function taking in ω0 and spits out u with time/space dependence (3d array)
     
     output = zeros(l+1,m,n)
     
